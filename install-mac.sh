c649f37def9a56e85335f25e813bd3e3c0ca6987
mohanaravind <mohanaravind@gmail.com>
2017-12-10 12:14:45 -0500

    Updated the main.cpp to be compatible with mac

--- install-mac.sh
+++ install-mac.sh
@@ -4,7 +4,8 @@ cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
 cd build
 OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
 cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
